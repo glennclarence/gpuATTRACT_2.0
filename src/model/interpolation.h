3b32b624bac11e108d506187e627c3b982d5a430
glennclarence <glenn.glashagen@gmail.com>
2020-06-10 14:54:58 +0200

    minor formatting

--- src/model/interpolation.h
+++ src/model/interpolation.h
@@ -31,11 +31,6 @@ typename TypeWrapper<REAL>::real4_t interpolate(
 {
 	using real4_t = typename TypeWrapper<REAL>::real4_t;
 
-//	printf("%f %f %f %f %f %f\n" ,
-//				grid->minDim().x, grid->minDim().y, grid->minDim().z,
-//				grid->maxDim().x, grid->maxDim().y, grid->maxDim().z);
-//	exit(1);
-
 	const int3 idx = grid->getIndex(pos);
 	VoxelOctet<REAL> voxel = grid->getVoxelByIndex(idx, type);
 
@@ -85,10 +80,6 @@ void potForce(
 					pot = interpolate(outerGrid, pos, type, charge);
 				}
 			} else {
-//				static int count = 0;
-//				if (++count < 50) {
-//					printf("%u\n", i);
-//				}
 				pot = interpolate(innerGrid, pos, type, charge);
 			}
 		}
@@ -104,28 +95,24 @@ void potForce(
 
 #ifdef CUDA
 
+    template<typename REAL>
+    void d_potForce (
+            unsigned blockSize,
+            unsigned gridSize,
+            const cudaStream_t &stream,
+            const d_IntrlpGrid<REAL>& inner,
+            const d_IntrlpGrid<REAL>& outer,
+            const d_Protein<REAL>& prot,
+            const unsigned& numDOFs,
+            const REAL* data_in_x,
+            const REAL* data_in_y,
+            const REAL* data_in_z,
+            REAL* data_out_x,
+            REAL* data_out_y,
+            REAL* data_out_z,
+            REAL* data_out_E);
 
-
-
-
-template<typename REAL>
-void d_potForce (
-		unsigned blockSize,
-		unsigned gridSize,
-		const cudaStream_t &stream,
-		const d_IntrlpGrid<REAL>& inner,
-		const d_IntrlpGrid<REAL>& outer,
-		const d_Protein<REAL>& prot,
-		const unsigned& numDOFs,
-		const REAL* data_in_x,
-		const REAL* data_in_y,
-		const REAL* data_in_z,
-		REAL* data_out_x,
-		REAL* data_out_y,
-		REAL* data_out_z,
-		REAL* data_out_E);
-
-#endif
+    #endif
 
 } // namespace
 
